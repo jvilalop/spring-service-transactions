0dd9fd86a8a67d2809916ae28baecb85021ac82b
jvila <jvila@LAPTOP-PJHNOA4T.home>
2020-01-21 08:50:55 +0100

    CucumberTest commit

---
+++ springboot-service-transactions/src/test/java/com/codechallenge/springboot/cucumber/bdd/CucumberTest.java
@@ -0,0 +1,27 @@
+package com.codechallenge.springboot.cucumber.bdd;
+
+import cucumber.api.CucumberOptions;
+
+import cucumber.api.junit.Cucumber;
+
+import org.junit.runner.RunWith;
+
+
+
+/**
+
+ * To run cucumber test
+
+ */
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "classpath:features", plugin = {"pretty",
+
+                                                            "json:target/cucumber-report.json"})
+
+public class CucumberTest {
+
+
+
+}
